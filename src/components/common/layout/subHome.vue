937fa93587eb1c7c13dd02e681e853a25022767f
liiffe <mmlife0727@gmail.com>
2020-11-05 10:27:35 +0800

    Create subHome.vue

---
+++ src/components/common/layout/subHome.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="wrapper">
+        <v-head></v-head>
+        <transition name="move" mode="out-in">
+            <keep-alive>
+                <router-view></router-view>
+            </keep-alive>
+        </transition>
+    </div>
+</template>
+
+<script>
+import vHead from './Header.vue';
+import bus from '../../../utils/bus';
+
+
+export default {
+    data() {
+        return {
+
+        };
+    },
+    components: {
+        vHead,
+    },
+    created() {
+
+    },
+    mounted(){
+        // bus.$emit('IDPermiss', {
+        //     hasIDpermiss:false
+        // })
+    }
+};
+</script>
