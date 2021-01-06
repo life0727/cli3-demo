2a0b6c6b60f838507e8e348719ace59bce361dc1
liiffe <mmlife0727@gmail.com>
2021-01-07 00:22:15 +0800

    Update Fetch.js

--- src/http/server/Fetch.js
+++ src/http/server/Fetch.js
@@ -100,8 +100,16 @@ export default function(config) {
       }]
     };
     service(options).then(response => {
-        //console.log('response', response)
-        options.context && options.context.loading && options.context.loading.close();
+        //console.log('response', response,options.context.loading)
+        //options.context && options.context.loading && options.context.loading.close();
+        if(options.context){
+            if(Array.isArray(options.context.loading)){
+                options.context.loading.length && options.context.loading.forEach(item => item.close());
+                options.context.loading = [];
+            }else{
+                options.context.loading && options.context.loading.close();
+            }
+        }
         if(response.redirectUrl){
             response.redirectTarget && response.redirectTarget == 'blank' ? window.open(response.redirectUrl) : window.location.href = response.redirectUrl
         }else{
@@ -111,9 +119,9 @@ export default function(config) {
                 window.location.href = response.data.redirectUrl
                 //setTimeout(window.open(response.data.redirectUrl), 500);//防止被浏览器拦截
             } else if (response.status == 200 && (!response.data.ret || response.data.ret == 'success')) { /// || => tools/goodcase/meetpoint/list?
-                config.success(response.data);      
+                config.success(response.data);   
             } else {
-                const callBackText = response.statusText || response.data.ret
+                const callBackText = response.statusText || response.data.ret;
                 if (config.error) {
                     config.error(callBackText);
                 } else {
@@ -142,37 +150,53 @@ export const _fetch = function(config){
             'Content-Type':config.headers || 'application/x-www-form-urlencoded'
         },
         transformRequest:!config.transformRequest ? [function(data){return data}] : [function (data) {
+            //console.log('transformRequest',data)
             let ret = ''
             for (let it in data) {
                 ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
             }
+            //console.log('ret',ret)
             return ret
            
       }]
     };
     return new Promise((resolve) =>{
         service(options).then(response => {
-            options.context && options.context.loading && options.context.loading.close();
-                if(response.redirectUrl){
-                    response.redirectTarget && response.redirectTarget == 'blank' ? window.open(response.redirectUrl) : window.location.href = response.redirectUrl
+            if(options.context){
+                if(Array.isArray(options.context.loading)){
+                    options.context.loading.length && options.context.loading.forEach(item => item.close());
+                    options.context.loading = [];
                 }else{
-                    if (response.data.ret == 'notLogin') { //未登录跳外链
-                        window.location.href = response.data.redirectUrl + window.location
-                    } else if (response.data.ret == 'notPermissions') { //没有权限跳外链
-                        window.location.href = response.data.redirectUrl
-                    } else if (response.status == 200 && (!response.data.ret || response.data.ret == 'success')) { /// || => tools/goodcase/meetpoint/list?
-                        resolve(response.data)
-                    } else {
-                        const callBackText = response.statusText || response.data.ret;
-                        if (config.error) {
-                            config.error(callBackText);
-                        } else {
-                            Message(callBackText);
-                        }
-                        //resolve(null)
-                    }
+                    options.context.loading && options.context.loading.close();
                 }
+            };
+            
+            if(response.redirectUrl){
+                response.redirectTarget && response.redirectTarget == 'blank' ? window.open(response.redirectUrl) : window.location.href = response.redirectUrl
+            }else{
+                if (response.data.ret == 'notLogin') { //未登录跳外链
+                    window.location.href = response.data.redirectUrl + window.location
+                } else if (response.data.ret == 'notPermissions') { //没有权限跳外链
+                    window.location.href = response.data.redirectUrl
+                } else if (response.status == 200 && (!response.data.ret || response.data.ret == 'success')) { /// || => tools/goodcase/meetpoint/list?
+                    //config.success(response.data);   
+                    resolve(response.data)
+                } else {
+                    const callBackText = response.statusText || response.data.msg || response.data.ret;
+                    if (config.error) {
+                        config.error(callBackText);
+                    } else {
+                        Message(callBackText);
+                    }
+                    //resolve(null)
+                }
+            }
         })
+        .catch(error => {
+            options.context && options.context.loading && options.context.loading.close();
+            //config.error ? config.error(error) : errorFun(error);
+            resolve(error)
+        });
         
     })
 }
