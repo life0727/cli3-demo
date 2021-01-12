cc4ff677b3866e9fb604e2927e5b31bce26400ab
liiffe <mmlife0727@gmail.com>
2021-01-12 21:54:35 +0800

    Create idTaskTable.vue

---
+++ src/components/page/demo/table_demo/idTaskTable.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="idTaskTable">
+      <span style="color:green">{{row[prop1]}}</span> <el-link type="primary" @click="linkTo(row)">{{ row[prop2]}}</el-link>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name:'idTaskTable',
+    props:{
+        row:{
+            type:Object,
+            default(){
+                return {}
+            }
+        },
+        prop1:{
+            type:String,
+            default:''
+        },
+        prop2:{
+            type:String,
+            default:''
+        }
+    },
+    data () {
+      return {
+      };
+    },
+    computed: {},
+    components: {},
+    watch: {},
+    mounted() {},
+    methods: {
+        linkTo(row){
+            console.log('linkTo',row)
+        }
+    }
+  }
+
+</script>
+<style lang='less' scoped>
+
+</style>
