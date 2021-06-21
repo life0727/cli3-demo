3921a00bcbee1bdd464122abb22fb775b9673a9d
liiffe <mmlife0727@gmail.com>
2021-06-21 15:04:24 +0800

    Create showPicList.vue

---
+++ src/components/page/home/showPicList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="showPicList">
+        <h1>showPicList</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'showPicList',
+    components:{
+    
+    },
+    data() {
+        return {
+
+        };
+    },
+    mounted(){
+
+    },
+    methods: {
+
+    }
+};
+</script>
+<style scoped lang="less">
+.showPicList{
+    position: relative;
+    height: 100%;
+   
+}
+</style>
