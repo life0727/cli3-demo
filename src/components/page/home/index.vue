97b301a6eb43e75e52ccd57450c7e2f91bd162bd
liiffe <mmlife0727@gmail.com>
2021-06-21 15:19:47 +0800

    Create index.vue

---
+++ src/components/page/home/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="index">
+        <h1>index</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'index',
+    components:{
+    
+    },
+    data() {
+        return {
+
+        };
+    },
+    mounted(){
+
+    },
+    methods: {
+
+    }
+};
+</script>
+<style scoped lang="less">
+.index{
+    position: relative;
+    height: 100%;
+   
+}
+</style>
