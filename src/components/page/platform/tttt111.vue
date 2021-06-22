3b71181dfcb9768539cf31bb8e4c9824d1e76340
liiffe <mmlife0727@gmail.com>
2021-06-22 14:40:43 +0800

    Create tttt111.vue

---
+++ src/components/page/platform/tttt111.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="tttt111">
+        <h1>tttt111</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'tttt111',
+    components:{
+    
+    },
+    data() {
+        return {
+
+        };
+    },
+    mounted(){
+
+    },
+    methods: {
+
+    }
+};
+</script>
+<style scoped lang="less">
+.tttt111{
+    position: relative;
+    height: 100%;
+   
+}
+</style>
