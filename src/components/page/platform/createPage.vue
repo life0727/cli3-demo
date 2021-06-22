f630545791a7e61d13395fa7488e053ea387d75b
liiffe <mmlife0727@gmail.com>
2021-06-22 10:57:27 +0800

    Create createPage.vue

---
+++ src/components/page/platform/createPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="createPage">
+        <h1>createPage</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'createPage',
+    components:{
+    
+    },
+    data() {
+        return {
+
+        };
+    },
+    mounted(){
+
+    },
+    methods: {
+
+    }
+};
+</script>
+<style scoped lang="less">
+.createPage{
+    position: relative;
+    height: 100%;
+   
+}
+</style>
