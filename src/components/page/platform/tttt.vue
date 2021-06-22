ef7f387cff2e193b8aa5769e4b3f5c7ac9093eee
liiffe <mmlife0727@gmail.com>
2021-06-22 14:41:03 +0800

    Create tttt.vue

---
+++ src/components/page/platform/tttt.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="tttt">
+        <h1>tttt</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'tttt',
+    components:{
+    
+    },
+    data() {
+        return {
+
+        };
+    },
+    mounted(){
+
+    },
+    methods: {
+
+    }
+};
+</script>
+<style scoped lang="less">
+.tttt{
+    position: relative;
+    height: 100%;
+   
+}
+</style>
